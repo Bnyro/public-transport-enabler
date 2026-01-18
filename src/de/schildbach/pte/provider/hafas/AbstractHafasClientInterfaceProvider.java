c6b08ca0a808fe5131dec47bc6a9cda4c355a051
santawho <196087559+santawho@users.noreply.github.com>
2026-01-18 10:58:12 +0100

    Hafas: accept error code "H9220_R" (whatever the _R is ...)

--- src/de/schildbach/pte/provider/hafas/AbstractHafasClientInterfaceProvider.java
+++ src/de/schildbach/pte/provider/hafas/AbstractHafasClientInterfaceProvider.java
@@ -851,7 +851,7 @@ public abstract class AbstractHafasClientInterfaceProvider extends AbstractHafas
                     return new QueryTripsResult(header, QueryTripsResult.Status.NO_TRIPS);
                 if ("H895".equals(err)) // Departure/Arrival are too near.
                     return new QueryTripsResult(header, QueryTripsResult.Status.TOO_CLOSE);
-                if ("H9220".equals(err)) // Nearby to the given address stations could not be found.
+                if ("H9220".equals(err) || "H9220_R".equals(err)) // Nearby to the given address stations could not be found.
                     return new QueryTripsResult(header, QueryTripsResult.Status.UNRESOLVABLE_ADDRESS);
                 if ("H886".equals(err)) // HAFAS Kernel: No connections found within the requested time
                     // interval.
