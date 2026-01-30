39a8de8318195bdc56d27aeaa467664a50b83857
santawho <196087559+santawho@users.noreply.github.com>
2026-01-30 21:46:15 +0100

    fix: DB providers use PTE GeoUtils instead of externals

--- src/de/schildbach/pte/util/GeoUtils.java
+++ src/de/schildbach/pte/util/GeoUtils.java
@@ -9,8 +9,8 @@ public class GeoUtils {
         final double lon1 = pointA.getLonAsDouble() * rad;
         final double lat2 = pointB.getLatAsDouble() * rad;
         final double lon2 = pointB.getLonAsDouble() * rad;
-        final double sinDLat = Math.sin(((lat2 - lat1) * rad) / 2);
-        final double sinDLon = Math.sin(((lon2 - lon1) * rad) / 2);
+        final double sinDLat = Math.sin((lat2 - lat1) / 2);
+        final double sinDLon = Math.sin((lon2 - lon1) / 2);
         final double a = sinDLat * sinDLat + Math.cos(lat1) * Math.cos(lat2) * sinDLon * sinDLon;
         final double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         return 6371000 * c;
